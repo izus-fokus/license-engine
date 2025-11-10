f0a7ead8f8fedb16fa5fcde382cd2d9987965147
Florian Fritze <florian.fritze@ub.uni-stuttgart.de>
2025-11-10 08:05:17 +0100

    reformatted DefaultApi

--- src/main/java/resus/licenseengine/fossology/api/DefaultApi.java
+++ src/main/java/resus/licenseengine/fossology/api/DefaultApi.java
@@ -28,70 +28,70 @@ import resus.licenseengine.fossology.model.TokenRequest;
  *
  */
 @Path("/")
-public interface DefaultApi  {
+public interface DefaultApi {
 
     /**
      * Get a login session registered (deprecated, use /tokens)
-     *
-     * Get a login session registered using the username and password 
+     * <p>
+     * Get a login session registered using the username and password
      *
      */
     @GET
     @Path("/auth")
-    @Produces({ "application/json" })
-    @Operation(summary = "Get a login session registered (deprecated, use /tokens)", tags={  })
-    @ApiResponses(value = { 
-        @ApiResponse(responseCode = "299", description = "Resource is deprecated. Use /tokens", content = @Content(schema = @Schema(implementation = DefaultResponse.class))),
-        @ApiResponse(responseCode = "404", description = "UserName or password incorrect", content = @Content(schema = @Schema(implementation = Info.class))),
-        @ApiResponse(responseCode = "200", description = "Some error occured. Check the \"message\"", content = @Content(schema = @Schema(implementation = Info.class))) })
-    public DefaultResponse authGet(@QueryParam("username")String username, @QueryParam("password")String password);
+    @Produces({"application/json"})
+    @Operation(summary = "Get a login session registered (deprecated, use /tokens)", tags = {})
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "299", description = "Resource is deprecated. Use /tokens", content = @Content(schema = @Schema(implementation = DefaultResponse.class))),
+            @ApiResponse(responseCode = "404", description = "UserName or password incorrect", content = @Content(schema = @Schema(implementation = Info.class))),
+            @ApiResponse(responseCode = "200", description = "Some error occured. Check the \"message\"", content = @Content(schema = @Schema(implementation = Info.class)))})
+    public DefaultResponse authGet(@QueryParam("username") String username, @QueryParam("password") String password);
 
     /**
      * Generate a new token
-     *
-     * Generate a new token to access REST API 
+     * <p>
+     * Generate a new token to access REST API
      *
      */
     @POST
     @Path("/tokens")
-    @Consumes({ "application/json", "application/x-www-form-urlencoded" })
-    @Produces({ "application/json" })
-    @Operation(summary = "Generate a new token", tags={  })
-    @ApiResponses(value = { 
-        @ApiResponse(responseCode = "201", description = "Token generated", content = @Content(schema = @Schema(implementation = DefaultResponse.class))),
-        @ApiResponse(responseCode = "404", description = "UserName or password incorrect", content = @Content(schema = @Schema(implementation = Info.class))),
-        @ApiResponse(responseCode = "200", description = "Some error occured. Check the \"message\"", content = @Content(schema = @Schema(implementation = Info.class))) })
+    @Consumes({"application/json", "application/x-www-form-urlencoded"})
+    @Produces({"application/json"})
+    @Operation(summary = "Generate a new token", tags = {})
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Token generated", content = @Content(schema = @Schema(implementation = DefaultResponse.class))),
+            @ApiResponse(responseCode = "404", description = "UserName or password incorrect", content = @Content(schema = @Schema(implementation = Info.class))),
+            @ApiResponse(responseCode = "200", description = "Some error occured. Check the \"message\"", content = @Content(schema = @Schema(implementation = Info.class)))})
     public DefaultResponse tokensPost(TokenRequest body);
 
     /**
      * Generate a new token
-     *
-     * Generate a new token to access REST API 
+     * <p>
+     * Generate a new token to access REST API
      *
      */
     @POST
     @Path("/tokens")
-    @Consumes({ "application/json", "application/x-www-form-urlencoded" })
-    @Produces({ "application/json" })
-    @Operation(summary = "Generate a new token", tags={  })
-    @ApiResponses(value = { 
-        @ApiResponse(responseCode = "201", description = "Token generated", content = @Content(schema = @Schema(implementation = DefaultResponse.class))),
-        @ApiResponse(responseCode = "404", description = "UserName or password incorrect", content = @Content(schema = @Schema(implementation = Info.class))),
-        @ApiResponse(responseCode = "200", description = "Some error occured. Check the \"message\"", content = @Content(schema = @Schema(implementation = Info.class))) })
-    public DefaultResponse tokensPost(@Multipart(value = "username")  String username, @Multipart(value = "password")  String password, @Multipart(value = "token_name")  String tokenName, @Multipart(value = "token_scope")  String tokenScope, @Multipart(value = "token_expire")  LocalDate tokenExpire);
+    @Consumes({"application/json", "application/x-www-form-urlencoded"})
+    @Produces({"application/json"})
+    @Operation(summary = "Generate a new token", tags = {})
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Token generated", content = @Content(schema = @Schema(implementation = DefaultResponse.class))),
+            @ApiResponse(responseCode = "404", description = "UserName or password incorrect", content = @Content(schema = @Schema(implementation = Info.class))),
+            @ApiResponse(responseCode = "200", description = "Some error occured. Check the \"message\"", content = @Content(schema = @Schema(implementation = Info.class)))})
+    public DefaultResponse tokensPost(@Multipart(value = "username") String username, @Multipart(value = "password") String password, @Multipart(value = "token_name") String tokenName, @Multipart(value = "token_scope") String tokenScope, @Multipart(value = "token_expire") LocalDate tokenExpire);
 
     /**
      * Get the current API information
-     *
-     * Get the current API version and supported authentication methods 
+     * <p>
+     * Get the current API version and supported authentication methods
      *
      */
     @GET
     @Path("/version")
-    @Produces({ "application/json" })
-    @Operation(summary = "Get the current API information", tags={  })
-    @ApiResponses(value = { 
-        @ApiResponse(responseCode = "200", description = "The version and security information", content = @Content(schema = @Schema(implementation = InlineResponse200.class))),
-        @ApiResponse(responseCode = "200", description = "Some error occured. Check the \"message\"", content = @Content(schema = @Schema(implementation = Info.class))) })
+    @Produces({"application/json"})
+    @Operation(summary = "Get the current API information", tags = {})
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "The version and security information", content = @Content(schema = @Schema(implementation = InlineResponse200.class))),
+            @ApiResponse(responseCode = "200", description = "Some error occured. Check the \"message\"", content = @Content(schema = @Schema(implementation = Info.class)))})
     public InlineResponse200 versionGet();
 }
