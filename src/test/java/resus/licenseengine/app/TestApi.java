e52292ce1d7fca29d7671161e132e0c78c087f2a
Florian Fritze <florian.fritze@ub.uni-stuttgart.de>
2025-11-13 07:51:40 +0100

    changed setup for test

--- src/test/java/resus/licenseengine/app/TestApi.java
+++ src/test/java/resus/licenseengine/app/TestApi.java
@@ -19,15 +19,16 @@ public class TestApi {
 
     @BeforeAll
     public static void setUpClass() {
+        setUpFossology("http://localhost:7100","fossy", "fossy");
     }
 
     @AfterAll
     public static void tearDownClass() {
     }
 
-    public void setUpFossology(String fossologyEndpoint,
-                               String fossologyUsername,
-                               String fossologyPassword) {
+    public static void setUpFossology(String fossologyEndpoint,
+                                      String fossologyUsername,
+                                      String fossologyPassword) {
         TestApi.fossologyEndpoint = fossologyEndpoint + "/repo/api/v1";
         TestApi.fossologyUsername = fossologyUsername;
         TestApi.fossologyPassword = fossologyPassword;
@@ -35,7 +36,6 @@ public class TestApi {
 
     @Test
     public void fossologyIsAvailable() throws Exception {
-        setUpFossology("http://localhost:7100","fossy", "fossy");
         FossologyClient fossologyClient = new FossologyClient(fossologyEndpoint, fossologyUsername, fossologyPassword);
         boolean isAvailable = fossologyClient.isAvailable();
         assertTrue(isAvailable);
