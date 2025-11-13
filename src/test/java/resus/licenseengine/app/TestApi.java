1ce89722f6043b63733bdebf24c4d0e40f9dcf0c
Florian Fritze <florian.fritze@ub.uni-stuttgart.de>
2025-11-13 12:38:36 +0100

    spring boot tests

--- src/test/java/resus/licenseengine/app/TestApi.java
+++ src/test/java/resus/licenseengine/app/TestApi.java
@@ -1,44 +1,74 @@
 package resus.licenseengine.app;
 
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
-import resus.licenseengine.fossology.client.FossologyClient;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import resus.licenseengine.app.model.Software;
 
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.logging.Logger;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class TestApi {
 
     private static final Logger logger = Logger.getLogger(TestApi.class.getCanonicalName());
 
-    private static String fossologyEndpoint;
-    private static String fossologyUsername;
-    private static String fossologyPassword;
-
     @BeforeAll
     public static void setUpClass() {
-        setUpFossology("http://localhost:7100","fossy", "fossy");
     }
 
     @AfterAll
     public static void tearDownClass() {
     }
 
-    public static void setUpFossology(String fossologyEndpoint,
-                                      String fossologyUsername,
-                                      String fossologyPassword) {
-        TestApi.fossologyEndpoint = fossologyEndpoint + "/repo/api/v1";
-        TestApi.fossologyUsername = fossologyUsername;
-        TestApi.fossologyPassword = fossologyPassword;
+    @Test
+    @Order(1)
+    public void fossologyIsAvailable() throws Exception {
+        boolean isAvailable = LicenseEngine.isFossologyAvailable();
+        assertTrue(isAvailable);
     }
 
     @Test
-    public void fossologyIsAvailable() throws Exception {
-        FossologyClient fossologyClient = new FossologyClient(fossologyEndpoint, fossologyUsername, fossologyPassword);
-        boolean isAvailable = fossologyClient.isAvailable();
-        assertTrue(isAvailable);
+    @Order(2)
+    public void postReplay() throws Exception {
+        ClassLoader classLoader = getClass().getClassLoader();
+        try (InputStream is = classLoader.getResourceAsStream("replay.json")) {
+            if (is == null) {
+                throw new IllegalArgumentException("Ressource nicht gefunden!");
+            }
+            String content = new String(is.readAllBytes(), StandardCharsets.UTF_8);
+            JsonElement obj = JsonParser.parseString(content);
+            JsonObject jsonObject = obj.getAsJsonObject();
+            Software software = new Software(jsonObject.get("id").getAsString(),
+                    jsonObject.get("name").getAsString(),
+                    jsonObject.get("url").getAsString());
+            LicenseEngine.startProcessing(software);
+            LicenseEngine.addSoftware("replay",software);
+        }
+    }
+
+    @Test
+    @Order(3)
+    public void getSoftware() {
+        Software softResponse= LicenseEngine.getSoftware("replay");
+        assertEquals("https://github.com/RePlay-DH/replay-dh-client.git",softResponse.getUrl());
+        Set<String> licenseSet = new HashSet<>(Arrays.asList("LGPL-2.1", "MIT", "CC-BY-SA-3.0"));
+        assertEquals(licenseSet,softResponse.getEffectiveLicenses());
+        assertEquals("replay", softResponse.getName());
+        assertEquals(550,softResponse.getFiles().intValue());
     }
 }
 
