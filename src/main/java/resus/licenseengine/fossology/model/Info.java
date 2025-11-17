7d380fcf6219cb8268bf13912ad5e3fed7a3ec87
Florian Fritze <florian.fritze@ub.uni-stuttgart.de>
2025-11-17 14:15:57 +0100

    two files reformatted

--- src/main/java/resus/licenseengine/fossology/model/Info.java
+++ src/main/java/resus/licenseengine/fossology/model/Info.java
@@ -6,130 +6,136 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonValue;
 import com.fasterxml.jackson.annotation.JsonCreator;
 
-public class Info   {
-  
-  @Schema(example = "200", description = "HTTP status code")
- /**
-   * HTTP status code  
-  **/
-  private Integer code = null;
-  
-  @Schema(description = "Message in the info")
- /**
-   * Message in the info  
-  **/
-  private String message = null;
-  public enum TypeEnum {
-    INFO("INFO"),
-    ERROR("ERROR");
+public class Info {
 
-    private String value;
+    @Schema(example = "200", description = "HTTP status code")
+    /**
+     * HTTP status code
+     **/
+    private Integer code = null;
 
-    TypeEnum(String value) {
-      this.value = value;
+    @Schema(description = "Message in the info")
+    /**
+     * Message in the info
+     **/
+    private String message = null;
+
+    public enum TypeEnum {
+        INFO("INFO"),
+        ERROR("ERROR");
+
+        private String value;
+
+        TypeEnum(String value) {
+            this.value = value;
+        }
+
+        @JsonValue
+        public String getValue() {
+            return value;
+        }
+
+        @Override
+        public String toString() {
+            return String.valueOf(value);
+        }
+
+        @JsonCreator
+        public static TypeEnum fromValue(String text) {
+            for (TypeEnum b : TypeEnum.values()) {
+                if (String.valueOf(b.value).equals(text)) {
+                    return b;
+                }
+            }
+            return null;
+        }
     }
-    @JsonValue
-    public String getValue() {
-      return value;
+
+    @Schema(description = "Denotes if info was created on error")
+    /**
+     * Denotes if info was created on error
+     **/
+    private TypeEnum type = null;
+
+    /**
+     * HTTP status code
+     *
+     * @return code
+     **/
+    @JsonProperty("code")
+    public Integer getCode() {
+        return code;
     }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+
+    public Info code(Integer code) {
+        this.code = code;
+        return this;
+    }
+
+    /**
+     * Message in the info
+     *
+     * @return message
+     **/
+    @JsonProperty("message")
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Info message(String message) {
+        this.message = message;
+        return this;
+    }
+
+    /**
+     * Denotes if info was created on error
+     *
+     * @return type
+     **/
+    @JsonProperty("type")
+    public String getType() {
+        if (type == null) {
+            return null;
+        }
+        return type.getValue();
+    }
+
+    public void setType(TypeEnum type) {
+        this.type = type;
+    }
+
+    public Info type(TypeEnum type) {
+        this.type = type;
+        return this;
+    }
+
 
     @Override
     public String toString() {
-      return String.valueOf(value);
+
+        return "class Info {\n" +
+                "    code: " + toIndentedString(code) + "\n" +
+                "    message: " + toIndentedString(message) + "\n" +
+                "    type: " + toIndentedString(type) + "\n" +
+                "}";
     }
-    @JsonCreator
-    public static TypeEnum fromValue(String text) {
-      for (TypeEnum b : TypeEnum.values()) {
-        if (String.valueOf(b.value).equals(text)) {
-          return b;
+
+    /**
+     * Convert the given object to string with each line indented by 4 spaces
+     * (except the first line).
+     */
+    private static String toIndentedString(java.lang.Object o) {
+        if (o == null) {
+            return "null";
         }
-      }
-      return null;
+        return o.toString().replace("\n", "\n    ");
     }
-  }  
-  @Schema(description = "Denotes if info was created on error")
- /**
-   * Denotes if info was created on error  
-  **/
-  private TypeEnum type = null;
- /**
-   * HTTP status code
-   * @return code
-  **/
-  @JsonProperty("code")
-  public Integer getCode() {
-    return code;
-  }
-
-  public void setCode(Integer code) {
-    this.code = code;
-  }
-
-  public Info code(Integer code) {
-    this.code = code;
-    return this;
-  }
-
- /**
-   * Message in the info
-   * @return message
-  **/
-  @JsonProperty("message")
-  public String getMessage() {
-    return message;
-  }
-
-  public void setMessage(String message) {
-    this.message = message;
-  }
-
-  public Info message(String message) {
-    this.message = message;
-    return this;
-  }
-
- /**
-   * Denotes if info was created on error
-   * @return type
-  **/
-  @JsonProperty("type")
-  public String getType() {
-    if (type == null) {
-      return null;
-    }
-    return type.getValue();
-  }
-
-  public void setType(TypeEnum type) {
-    this.type = type;
-  }
-
-  public Info type(TypeEnum type) {
-    this.type = type;
-    return this;
-  }
-
-
-  @Override
-  public String toString() {
-    StringBuilder sb = new StringBuilder();
-    sb.append("class Info {\n");
-    
-    sb.append("    code: ").append(toIndentedString(code)).append("\n");
-    sb.append("    message: ").append(toIndentedString(message)).append("\n");
-    sb.append("    type: ").append(toIndentedString(type)).append("\n");
-    sb.append("}");
-    return sb.toString();
-  }
-
-  /**
-   * Convert the given object to string with each line indented by 4 spaces
-   * (except the first line).
-   */
-  private static String toIndentedString(java.lang.Object o) {
-    if (o == null) {
-      return "null";
-    }
-    return o.toString().replace("\n", "\n    ");
-  }
 }
--- src/main/java/resus/licenseengine/fossology/model/VcsUpload.java
+++ src/main/java/resus/licenseengine/fossology/model/VcsUpload.java
@@ -7,208 +7,216 @@ import com.fasterxml.jackson.annotation.JsonValue;
 import com.fasterxml.jackson.annotation.JsonCreator;
 
 /**
-  * To create an upload from a version control system
+ * To create an upload from a version control system
  **/
-@Schema(description="To create an upload from a version control system")
-public class VcsUpload  implements OneOfbody  {
-  public enum VcsTypeEnum {
-    SVN("svn"),
-    GIT("git");
+@Schema(description = "To create an upload from a version control system")
+public class VcsUpload implements OneOfbody {
+    public enum VcsTypeEnum {
+        SVN("svn"),
+        GIT("git");
 
-    private String value;
+        private String value;
 
-    VcsTypeEnum(String value) {
-      this.value = value;
+        VcsTypeEnum(String value) {
+            this.value = value;
+        }
+
+        @JsonValue
+        public String getValue() {
+            return value;
+        }
+
+        @Override
+        public String toString() {
+            return String.valueOf(value);
+        }
+
+        @JsonCreator
+        public static VcsTypeEnum fromValue(String text) {
+            for (VcsTypeEnum b : VcsTypeEnum.values()) {
+                if (String.valueOf(b.value).equals(text)) {
+                    return b;
+                }
+            }
+            return null;
+        }
     }
-    @JsonValue
-    public String getValue() {
-      return value;
+
+    @Schema(required = true, description = "VCS type")
+    /**
+     * VCS type
+     **/
+    private VcsTypeEnum vcsType = null;
+
+    @Schema(required = true, description = "URL of the repository")
+    /**
+     * URL of the repository
+     **/
+    private String vcsUrl = null;
+
+    @Schema(description = "Branch to checkout for analysis (for Git only)")
+    /**
+     * Branch to checkout for analysis (for Git only)
+     **/
+    private String vcsBranch = null;
+
+    @Schema(description = "Display name of the upload")
+    /**
+     * Display name of the upload
+     **/
+    private String vcsName = null;
+
+    @Schema(description = "Username for the VCS")
+    /**
+     * Username for the VCS
+     **/
+    private String vcsUsername = null;
+
+    @Schema(description = "Password for the VCS")
+    /**
+     * Password for the VCS
+     **/
+    private String vcsPassword = null;
+
+    /**
+     * VCS type
+     *
+     * @return vcsType
+     **/
+    @JsonProperty("vcsType")
+    public String getVcsType() {
+        if (vcsType == null) {
+            return null;
+        }
+        return vcsType.getValue();
     }
+
+    public void setVcsType(VcsTypeEnum vcsType) {
+        this.vcsType = vcsType;
+    }
+
+    public VcsUpload vcsType(VcsTypeEnum vcsType) {
+        this.vcsType = vcsType;
+        return this;
+    }
+
+    /**
+     * URL of the repository
+     *
+     * @return vcsUrl
+     **/
+    @JsonProperty("vcsUrl")
+    public String getVcsUrl() {
+        return vcsUrl;
+    }
+
+    public void setVcsUrl(String vcsUrl) {
+        this.vcsUrl = vcsUrl;
+    }
+
+    public VcsUpload vcsUrl(String vcsUrl) {
+        this.vcsUrl = vcsUrl;
+        return this;
+    }
+
+    /**
+     * Branch to checkout for analysis (for Git only)
+     *
+     * @return vcsBranch
+     **/
+    @JsonProperty("vcsBranch")
+    public String getVcsBranch() {
+        return vcsBranch;
+    }
+
+    public void setVcsBranch(String vcsBranch) {
+        this.vcsBranch = vcsBranch;
+    }
+
+    public VcsUpload vcsBranch(String vcsBranch) {
+        this.vcsBranch = vcsBranch;
+        return this;
+    }
+
+    /**
+     * Display name of the upload
+     *
+     * @return vcsName
+     **/
+    @JsonProperty("vcsName")
+    public String getVcsName() {
+        return vcsName;
+    }
+
+    public void setVcsName(String vcsName) {
+        this.vcsName = vcsName;
+    }
+
+    public VcsUpload vcsName(String vcsName) {
+        this.vcsName = vcsName;
+        return this;
+    }
+
+    /**
+     * Username for the VCS
+     *
+     * @return vcsUsername
+     **/
+    @JsonProperty("vcsUsername")
+    public String getVcsUsername() {
+        return vcsUsername;
+    }
+
+    public void setVcsUsername(String vcsUsername) {
+        this.vcsUsername = vcsUsername;
+    }
+
+    public VcsUpload vcsUsername(String vcsUsername) {
+        this.vcsUsername = vcsUsername;
+        return this;
+    }
+
+    /**
+     * Password for the VCS
+     *
+     * @return vcsPassword
+     **/
+    @JsonProperty("vcsPassword")
+    public String getVcsPassword() {
+        return vcsPassword;
+    }
+
+    public void setVcsPassword(String vcsPassword) {
+        this.vcsPassword = vcsPassword;
+    }
+
+    public VcsUpload vcsPassword(String vcsPassword) {
+        this.vcsPassword = vcsPassword;
+        return this;
+    }
+
 
     @Override
     public String toString() {
-      return String.valueOf(value);
+
+        return "class VcsUpload {\n" +
+                "    vcsType: " + toIndentedString(vcsType) + "\n" +
+                "    vcsUrl: " + toIndentedString(vcsUrl) + "\n" +
+                "    vcsBranch: " + toIndentedString(vcsBranch) + "\n" +
+                "    vcsName: " + toIndentedString(vcsName) + "\n" +
+                "    vcsUsername: " + toIndentedString(vcsUsername) + "\n" +
+                "    vcsPassword: " + toIndentedString(vcsPassword) + "\n" +
+                "}";
     }
-    @JsonCreator
-    public static VcsTypeEnum fromValue(String text) {
-      for (VcsTypeEnum b : VcsTypeEnum.values()) {
-        if (String.valueOf(b.value).equals(text)) {
-          return b;
+
+    /**
+     * Convert the given object to string with each line indented by 4 spaces
+     * (except the first line).
+     */
+    private static String toIndentedString(java.lang.Object o) {
+        if (o == null) {
+            return "null";
         }
-      }
-      return null;
+        return o.toString().replace("\n", "\n    ");
     }
-  }  
-  @Schema(required = true, description = "VCS type")
- /**
-   * VCS type  
-  **/
-  private VcsTypeEnum vcsType = null;
-  
-  @Schema(required = true, description = "URL of the repository")
- /**
-   * URL of the repository  
-  **/
-  private String vcsUrl = null;
-  
-  @Schema(description = "Branch to checkout for analysis (for Git only)")
- /**
-   * Branch to checkout for analysis (for Git only)  
-  **/
-  private String vcsBranch = null;
-  
-  @Schema(description = "Display name of the upload")
- /**
-   * Display name of the upload  
-  **/
-  private String vcsName = null;
-  
-  @Schema(description = "Username for the VCS")
- /**
-   * Username for the VCS  
-  **/
-  private String vcsUsername = null;
-  
-  @Schema(description = "Password for the VCS")
- /**
-   * Password for the VCS  
-  **/
-  private String vcsPassword = null;
- /**
-   * VCS type
-   * @return vcsType
-  **/
-  @JsonProperty("vcsType")
-  public String getVcsType() {
-    if (vcsType == null) {
-      return null;
-    }
-    return vcsType.getValue();
-  }
-
-  public void setVcsType(VcsTypeEnum vcsType) {
-    this.vcsType = vcsType;
-  }
-
-  public VcsUpload vcsType(VcsTypeEnum vcsType) {
-    this.vcsType = vcsType;
-    return this;
-  }
-
- /**
-   * URL of the repository
-   * @return vcsUrl
-  **/
-  @JsonProperty("vcsUrl")
-  public String getVcsUrl() {
-    return vcsUrl;
-  }
-
-  public void setVcsUrl(String vcsUrl) {
-    this.vcsUrl = vcsUrl;
-  }
-
-  public VcsUpload vcsUrl(String vcsUrl) {
-    this.vcsUrl = vcsUrl;
-    return this;
-  }
-
- /**
-   * Branch to checkout for analysis (for Git only)
-   * @return vcsBranch
-  **/
-  @JsonProperty("vcsBranch")
-  public String getVcsBranch() {
-    return vcsBranch;
-  }
-
-  public void setVcsBranch(String vcsBranch) {
-    this.vcsBranch = vcsBranch;
-  }
-
-  public VcsUpload vcsBranch(String vcsBranch) {
-    this.vcsBranch = vcsBranch;
-    return this;
-  }
-
- /**
-   * Display name of the upload
-   * @return vcsName
-  **/
-  @JsonProperty("vcsName")
-  public String getVcsName() {
-    return vcsName;
-  }
-
-  public void setVcsName(String vcsName) {
-    this.vcsName = vcsName;
-  }
-
-  public VcsUpload vcsName(String vcsName) {
-    this.vcsName = vcsName;
-    return this;
-  }
-
- /**
-   * Username for the VCS
-   * @return vcsUsername
-  **/
-  @JsonProperty("vcsUsername")
-  public String getVcsUsername() {
-    return vcsUsername;
-  }
-
-  public void setVcsUsername(String vcsUsername) {
-    this.vcsUsername = vcsUsername;
-  }
-
-  public VcsUpload vcsUsername(String vcsUsername) {
-    this.vcsUsername = vcsUsername;
-    return this;
-  }
-
- /**
-   * Password for the VCS
-   * @return vcsPassword
-  **/
-  @JsonProperty("vcsPassword")
-  public String getVcsPassword() {
-    return vcsPassword;
-  }
-
-  public void setVcsPassword(String vcsPassword) {
-    this.vcsPassword = vcsPassword;
-  }
-
-  public VcsUpload vcsPassword(String vcsPassword) {
-    this.vcsPassword = vcsPassword;
-    return this;
-  }
-
-
-  @Override
-  public String toString() {
-    StringBuilder sb = new StringBuilder();
-    sb.append("class VcsUpload {\n");
-    
-    sb.append("    vcsType: ").append(toIndentedString(vcsType)).append("\n");
-    sb.append("    vcsUrl: ").append(toIndentedString(vcsUrl)).append("\n");
-    sb.append("    vcsBranch: ").append(toIndentedString(vcsBranch)).append("\n");
-    sb.append("    vcsName: ").append(toIndentedString(vcsName)).append("\n");
-    sb.append("    vcsUsername: ").append(toIndentedString(vcsUsername)).append("\n");
-    sb.append("    vcsPassword: ").append(toIndentedString(vcsPassword)).append("\n");
-    sb.append("}");
-    return sb.toString();
-  }
-
-  /**
-   * Convert the given object to string with each line indented by 4 spaces
-   * (except the first line).
-   */
-  private static String toIndentedString(java.lang.Object o) {
-    if (o == null) {
-      return "null";
-    }
-    return o.toString().replace("\n", "\n    ");
-  }
 }
